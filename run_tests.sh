e21cbcd3a8f7fcfa1454804b1068c0bc10feb6b5
slava <slava@satellogic.com>
2017-05-05 02:37:27 +0300

    skeleton

--- run_tests.sh
+++ run_tests.sh
@@ -3,6 +3,6 @@
 set -e
 
 python3 -m coverage erase
-python3 -m coverage run --branch --source diq -m unittest discover -v tests/
+python3 -m coverage run --branch --source subsync -m unittest discover -v tests/
 python3 -m coverage report
 python3 -m coverage erase
