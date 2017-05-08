dbe935b655c8c444706316d8181e9e5045f53715
slava <slava@satellogic.com>
2017-05-08 03:05:27 +0300

    + tests

--- tests/subtitles/test_opensubtitles.py
+++ tests/subtitles/test_opensubtitles.py
@@ -1,7 +1,7 @@
 import unittest
 from tests.test_utils import run_integration_tests
 
-from subsync.subtitles.opensubtitles import OSCrawler
+from subsync.subtitles.opensubtitles import OSCrawler, SubsOrganizer
 
 
 imdb_id = '0060827'  # persona
@@ -14,7 +14,17 @@ class TestOSCrawler(unittest.TestCase):
     def test_imdb_to_os(self):
         crawler = OSCrawler()
         crawler.imdb_id_to_os_id(imdb_id)
+        subs = crawler.imdb_id_to_subs(imdb_id)
+        print(subs)
 
     def test_download(self):
         crawler = OSCrawler(download_folder='/home/slava/data/subs')
-        crawler.download(range(int(os_id), int(os_id)+11))
+        subs = crawler.imdb_id_to_subs(imdb_id)
+        crawler.download(subs)
+
+
+@unittest.skipUnless(run_integration_tests(), '')
+class TestSubsOrganizer(unittest.TestCase):
+    def test_organize(self):
+        organizer = SubsOrganizer()
+        organizer.process_folder('/home/slava/Downloads', '/home/slava/data/subs')
