43d52c1bf9a23352296f7e8290a21a276dcf4f96
slava <slava@satellogic.com>
2017-05-19 04:59:05 +0300

    more Signature methods

--- subsync/subtitles/sub_signature.py
+++ subsync/subtitles/sub_signature.py
@@ -31,3 +31,67 @@ class SubSignature:
     def __iter__(self):
         for interval in self.intervals:
             yield interval
+
+    def __eq__(self, other):
+        """ equals if intervals are exactly the same"""
+        return self.intervals == other.intervals
+
+    def __add__(self, shift_ms):
+        """ shifts signature forward by that many milliseconds. """
+        intervals = [(interval[0] + shift_ms, interval[1] + shift_ms) for interval in self]
+        return SubSignature(intervals=intervals)
+
+    def __radd__(self, shift_ms):
+        return self.__add__(shift_ms)
+
+    def __mul__(self, factor):
+        """ applies gains on signature. """
+        intervals = [(interval[0] * factor, interval[1] * factor) for interval in self]
+        return SubSignature(intervals=intervals)
+
+    def __rmul__(self, factor):
+        return self.__mul__(factor)
+
+    def __str__(self):
+        return '\n'.join(['%s -> %s' % (interval[0], interval[1]) for interval in self.intervals])
+
+    def _sort(self):
+        sorted = self.intervals.copy()
+        sorted.sort(key=lambda interval: interval[0])
+        return SubSignature(intervals=sorted)
+
+    def _simplify(self):
+        """ merges overlapping intervals. assumes sorted. updates self. """
+        simple = []
+        for interval in self:
+            if len(simple) == 0:
+                simple += [interval]
+            else:
+                if interval[0] < simple[-1][1]:  # intersect last interval => merging
+                    simple[-1] = (simple[-1][0], interval[1])
+                else:
+                    simple += [interval]
+        return SubSignature(intervals=simple)
+
+    def total_time(self):
+        return sum([interval[1] - interval[0] for interval in self])
+
+    def merge(self, other):
+        raise NotImplementedError  # TODO
+
+    def intersect(self, other):
+        raise NotImplementedError  # TODO
+
+    def dist(self, other):
+        """
+        metric between signatures - intersection over union, Jaccard index
+        :param other: SubSignature
+        :return: [0..1], or None if any of signatures is empty
+        """
+        if not self.intervals or not other.intervals:
+            return None
+
+        intersection_time = self.intersect(other).total_time()
+        union_time = self.merge(other).total_time()
+
+        return intersection_time / union_time
--- tests/subtitles/test_sub_signature.py
+++ tests/subtitles/test_sub_signature.py
@@ -22,3 +22,38 @@ class TestSubSignature(unittest.TestCase):
 
     def test_iter(self):
         self.assertEqual(len(list(self.sig)), len(self.sig))
+
+    def test_total_time(self):
+        self.assertEqual(self.sig.total_time(), 48165)
+
+    def test_eq(self):
+        self.assertEqual(self.sig, self.sig)
+
+        self.assertNotEqual(self.sig, self.sig + 0.1)
+
+    def test_add(self):
+        shifted = 22 + self.sig + 20
+        self.assertAlmostEqual(shifted.intervals[0][0], self.sig.intervals[0][0] + 42)
+        self.assertAlmostEqual(shifted.total_time(), self.sig.total_time())
+
+    def test_mul(self):
+        shifted = 2 * self.sig * 3
+        self.assertAlmostEqual(shifted.intervals[0][0], self.sig.intervals[0][0] * 6)
+        self.assertAlmostEqual(shifted.total_time(), self.sig.total_time() * 6)
+
+    def test_simplify(self):
+        sig = SubSignature(intervals=[(1, 2), (1.5, 3), (4, 5), (5, 6), (5.5, 6)])
+        expected_simplified = SubSignature(intervals=[(1, 3), (4, 5), (5, 6)])
+        self.assertEqual(sig._simplify(), expected_simplified)
+
+    def test_sort(self):
+        sig = SubSignature(intervals=[(1, 2), (1.5, 3), (0, 0.5), (-2, -1), (0.5, 16)])
+        expected_sorted = SubSignature(intervals=[(-2, -1), (0, 0.5), (0.5, 16), (1, 2), (1.5, 3)])
+        self.assertEqual(sig._sort(), expected_sorted)
+
+    def test_dist_empty(self):
+        empty = SubSignature()
+        self.assertIsNone(self.sig.dist(empty))
+
+    # def test_dist_same(self):
+    #     self.assertEqual(1, self.sig.dist(self.sig))
