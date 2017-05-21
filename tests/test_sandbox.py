9a93a9358dc65fb45822583e807743ba37de4427
slava <slava@satellogic.com>
2017-05-22 01:05:02 +0300

    sandbox

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -1,18 +1,23 @@
 import unittest
 import os
+from glob import glob
 import json
 import warnings
 from tqdm import tqdm
+from collections import OrderedDict
 from tempfile import TemporaryDirectory
 
 from pysrt import SubRipFile
 from pydub import AudioSegment
 
 from tests.test_utils import run_integration_tests
+import subsync.subtitles.sub_signature
 from subsync.subtitles.sub_signature import SubSignature
 from subsync.subtitles.subtitle import Subtitles
+from subsync.utils.audio import make_mono, downsample
 from subsync.download.youtube_downloader import YoutubeDownloader
 from subsync.vad.vad_marsbroshok import VADMarsbroshok
+from subsync.vad.vad_webrtc import VADWebrtc
 
 
 youtube_test_config = {'format': 'bestaudio/best', 'quiet': True}
@@ -32,26 +37,54 @@ class Download(unittest.TestCase):
 
 # @unittest.skip('slow real test')
 class VAD(unittest.TestCase):
-    def test_vad_marsbroshok(self):
+    def test_vad(self):
         signature = dict()
-        for id in persona_youtube_ids:
-            # audio_path = os.path.join(base_folder, 'persona/audio/%s.mp3' % id)
-            # audio = AudioSegment.from_file(audio_path)
-            # audio[:10 * 60 * 1000].export(os.path.join(base_folder, 'persona/audio/crop_%s.wav' % id), format='wav')
+        dest_rate = 8000
+        for id in persona_youtube_ids[:1]:  # HACK
+            audio_path = os.path.join(base_folder, 'persona/audio/%s.mp3' % id)
+            audio = AudioSegment.from_file(audio_path)
+            cropped_path = os.path.join(base_folder, 'persona/audio/crop_%s.wav' % id)
+            if not os.path.exists(cropped_path):
+                print('converting to wav:')
+                audio[:10 * 60 * 1000].export(cropped_path, format='wav')
 
-            audio_path = os.path.join(base_folder, 'persona/audio/crop_%s.wav' % id)
-            signature[id] = VADMarsbroshok().process(audio_path)
-            signature[id].write(os.path.join(base_folder, 'persona/audio/signature_%s.txt' % id))
+            downsampled_cropped_path = os.path.join(base_folder, 'persona/audio/downsampled_crop_%s.wav' % id)
+            if not os.path.exists(downsampled_cropped_path):
+                print('downsampling from %s to %s:' % (audio.frame_rate, dest_rate))
+                downsample(cropped_path, downsampled_cropped_path, audio.frame_rate, dest_rate)
 
-            clips_folder = os.path.join(base_folder, 'persona/audio/speech_clips/%s' % id)
-            print(signature[id])
-            signature[id].store_audio_clips(audio_path, clips_folder)
+            mono_downsampled_cropped_path = os.path.join(base_folder, 'persona/audio/mono_downsampled_crop_%s.wav' % id)
+            if not os.path.exists(mono_downsampled_cropped_path):
+                print('making mono:')
+                make_mono(downsampled_cropped_path, mono_downsampled_cropped_path)
+
+            print('extracting signature:')
+            signature[id] = VADMarsbroshok().process(mono_downsampled_cropped_path)
+            signature[id].write(os.path.join(base_folder, 'persona/audio/mb_%s.txt' % id))
+
+            for agressiveness in range(4):
+                signature[id] = VADWebrtc(agressiveness=agressiveness).process(mono_downsampled_cropped_path)
+                signature[id].write(os.path.join(base_folder, 'persona/audio/webrtc%s_%s.txt' % (agressiveness, id)))
+
+            # print('storing clips:')
+            # clips_folder = os.path.join(base_folder, 'persona/audio/speech_clips/%s' % id)
+            # print(signature[id])
+            # signature[id].store_audio_clips(mono_downsampled_cropped_path, clips_folder)
         # VADMarsbroshok(audio_path).plot_detected_speech_regions()
+
+    def test_visualise(self):
+        folder = os.path.join(base_folder, 'persona/audio/sig')
+        signatures = OrderedDict()
+        for sig_path in sorted(glob(os.path.join(folder, '*.txt'))):
+        # sig_path = os.path.join(base_folder, 'persona/audio/webrtc_signature_RuBbvBPYCDU.txt')
+            label = os.path.splitext(os.path.basename(sig_path))[0]
+            signatures[label] = SubSignature.read(sig_path)
+        subsync.subtitles.sub_signature.plot(signatures)
+
 
 
 @unittest.skip('slow real test')
 class Fit(unittest.TestCase):
-    @unittest.skip('slow real test')
     def test_fit_persona(self):
         warnings.simplefilter("ignore")
         index_path = os.path.join(base_folder, 'subs.json')
