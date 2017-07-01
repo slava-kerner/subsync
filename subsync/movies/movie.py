2a101c8bc7f65a6787f344ab25fa61b9b138e14a
slava <slava@satellogic.com>
2017-07-01 03:20:09 +0300

    elaborate on Movie

--- subsync/movies/movie.py
+++ subsync/movies/movie.py
@@ -7,3 +7,34 @@ class Movie:
         self.name = name
         self.imdb_id = imdb_id
         self.metadata = metadata or dict()
+
+
+class MovieYoutube:
+    """ represents 1 youtube """
+    def __init__(self, id=None, movie=None, lng_audio=None, captions=None):
+        """
+        :param id: youtube_id 
+        :param movie: instance of Movie 
+        :param lng_audio:
+        :param captions: [languages]
+        """
+        self.id = id
+        self.movie = movie
+        self.lng_audio = lng_audio
+        self.captions = captions
+        
+
+class MovieFull:
+    """
+    represents 1 movie with youtube instances 
+    """
+    def __init__(self, movie, videos=None, subs=None):
+        """
+        
+        :param movie: Movie 
+        :param videos: [MovieYoutube]
+        :param subs: MovieSubs
+        """
+        self.movie = movie
+        self.videos = videos
+        self.subs = subs
